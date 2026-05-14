8bb9fc2f4d3762445fd44a9a180c77f0da0b993a
lee <lijyeo@gmail.com>
2026-05-14 21:26:11 +0900

    feat: OAuth2 failure handler 추가 및 소셜 로그인 에러 처리

---
+++ src/main/java/com/market/config/OAuth2FailureHandler.java
@@ -0,0 +1,13 @@
+package com.market.config;
+
+/**
+ * <pre>
+ * com.market.config
+ * OAuth2FailureHandler
+ *  
+ * author : lee
+ * date   : 26. 5. 14.
+ * 설명    :
+ * </pre>
+ **/public class OAuth2FailureHandler {
+}
